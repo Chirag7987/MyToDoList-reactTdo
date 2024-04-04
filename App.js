81342d37b8b37b5598598ce000eb4b70af9eca59
Chirag Thakur <96881598+Chirag7987@users.noreply.github.com>
2024-04-05 03:22:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturants from './componets/basics/Resturants';
+import UseState from './componets/Hooks/useState';
+import UseEffect from './componets/Hooks/useEffect';
+import UseReducer from './componets/Hooks/useReducer';
+import Todo from './componets/todoreact/todo';
+import Temp from './componets/WeatherApi/temp';
+const App = () => {
+ return <Temp/>   
+}
+
+export default App;
